0a0158ccf4f2c25c7c80077e368c01f44fdf342e
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-13 23:17:32 +0530

    Create 10  numeric buttons

--- calculator.py
+++ calculator.py
@@ -28,7 +28,43 @@ def createCommandButton(root, text, command):
     return my_button
 
 
+def button_0_func(root):
+    return
+
+
 def button_1_func(root):
+    return
+
+
+def button_2_func(root):
+    return
+
+
+def button_3_func(root):
+    return
+
+
+def button_4_func(root):
+    return
+
+
+def button_5_func(root):
+    return
+
+
+def button_6_func(root):
+    return
+
+
+def button_7_func(root):
+    return
+
+
+def button_8_func(root):
+    return
+
+
+def button_9_func(root):
     return
 
 
@@ -40,10 +76,55 @@ def main():
     entry = createWideEntry(root_calc, width=35)
     entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+    button_0 = createCommandButton(
+        root=root_calc,
+        text='0',
+        command=button_0_func
+    )
     button_1 = createCommandButton(
         root=root_calc,
         text='1',
         command=button_1_func
+    )
+    button_2 = createCommandButton(
+        root=root_calc,
+        text='2',
+        command=button_2_func
+    )
+    button_3 = createCommandButton(
+        root=root_calc,
+        text='3',
+        command=button_3_func
+    )
+    button_4 = createCommandButton(
+        root=root_calc,
+        text='4',
+        command=button_4_func
+    )
+    button_5 = createCommandButton(
+        root=root_calc,
+        text='5',
+        command=button_5_func
+    )
+    button_6 = createCommandButton(
+        root=root_calc,
+        text='6',
+        command=button_6_func
+    )
+    button_7 = createCommandButton(
+        root=root_calc,
+        text='7',
+        command=button_7_func
+    )
+    button_8 = createCommandButton(
+        root=root_calc,
+        text='8',
+        command=button_8_func
+    )
+    button_9 = createCommandButton(
+        root=root_calc,
+        text='9',
+        command=button_9_func
     )
 
     root_calc.mainloop()
