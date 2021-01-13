443e41569dab8526172cf0f72806cb0d93765dc7
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-13 23:03:20 +0530

    Initial setup for calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+import tkinter
+
+
+def tkinterSetup():
+    root = tkinter.Tk()
+    return root
+
+
+def createLabel(root, text):
+    my_label = tkinter.Label(
+        master=root,
+        text=text
+    )
+    return my_label
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
