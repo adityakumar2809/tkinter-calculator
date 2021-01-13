4caf5b8cdb3fea4d2d20df9a2e6b816e68cb2194
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-13 23:28:10 +0530

    Position add, clear and equal buttons

--- calculator.py
+++ calculator.py
@@ -173,6 +173,9 @@ def main():
     button_9.grid(row=1, column=2)
 
     button_0.grid(row=4, column=0)
+    button_clear.grid(row=4, column=1)
+    button_add.grid(row=5, column=0)
+    button_equal.grid(row=5, column=1)
 
     root_calc.mainloop()
 
