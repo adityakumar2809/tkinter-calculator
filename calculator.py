b5f063affbe81042af82fc443d48bf05b91fa70d
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-13 23:06:54 +0530

    Add feature to take input

--- calculator.py
+++ calculator.py
@@ -14,10 +14,17 @@ def createLabel(root, text):
     return my_label
 
 
+def createWideEntry(root, width=60):
+    entry = tkinter.Entry(root, width=width, borderwidth=5)
+    return entry
+
+
 def main():
 
     root_calc = tkinterSetup()
     root_calc.title('Simple Calculator')
+
+    entry = createWideEntry(root_calc, width=35)
 
 
 if __name__ == "__main__":
